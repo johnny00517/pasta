4f4f94bb83cd60e5dcfb2f7259cb564329d5cf9b
Johnny Zhong <johnny00517@gmail.com>
2019-11-27 21:48:53 +0800

    add header

--- pasta.py
+++ pasta.py
@@ -11,5 +11,14 @@ while True:
 
 print(food)
 
-for f in food:
-	print('商品:', f[0], '為', f[1], '元')
+for foood in food:
+	print('商品:', foood[0], '為', foood[1], '元')
+
+with open('pasta.txt', 'w')as f:
+	for foood in food:
+		f.write(foood[0] + ',' + foood[1] + '\n')
+
+with open('pasta.csv', 'w', encoding='utf-8')as f:        #.cvs可以用excel打開  #encoding='utf-8'  編碼
+	f.write('商品,價格\n')
+	for foood in food:
+		f.write(foood[0] + ',' + foood[1] + '\n')
